863edc40d1b7045db3485ad4e6fbd8137621b33d
6878762 <USUARIOS\6878762@lab0-00.grad.icmc.usp.br>
2013-09-13 17:31:09 -0300

    asd

--- notebook/codes/geometry/lineinter.c
+++ notebook/codes/geometry/lineinter.c
@@ -1,3 +1,9 @@
+/* intersecao de linhas, na forma ax + by + c = 0:
+   X = DET |B1 C1| / DET |A1 B1|
+   	   |B2 C2|       |A2 B2|
+   Y = DET |A1 C1| / DET |B1 A1|
+           |A2 C2|       |B2 A2|
+*/
 void lines_inter(L& l1, L& l2)
 {
 	if (fabs(l1.a*l2.b-l2.a*l1.b) < EPS) return; // paralelas
--- notebook/codes/geometry/lineinter2.c
+++ notebook/codes/geometry/lineinter2.c
@@ -1,3 +1,18 @@
+/* intersecao de linhas dado dois pontos da linha, 
+   reta na forma = s*u+v; u = (p2-p1)/norm; v = p1
+   D = DET |DET |x1 1| DET |y1 1||
+           |    |x2 1|     |y2 1||
+           |DET |x3 1| DET |y3 1||
+           |	|x4 1|     |y4 1||
+   X = DET |DET |x1 y1| DET |x1 1|| / D
+           |    |x2 y2|     |x2 1||
+           |DET |x3 y3| DET |x3 1||
+           |	|x4 y4|     |x4 1||
+   Y = DET |DET |x1 y1| DET |y1 1|| / D
+           |    |x2 y2|     |y2 1||
+           |DET |x3 y3| DET |y3 1||
+           |	|x4 y4|     |y4 1||
+*/
 void lines_inter(P& p1, P& p2, P& p3, P& p4)
 {
 	double d = (p1.x-p2.x)*(p3.y-p4.y)-(p1.y-p2.y)*(p3.x-p4.x);
